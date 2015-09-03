4d47ced4034ec6586555b2e2198d56f333bda409
Rainbow <samantha@smsittech.com>
2015-09-03 10:00:33 -0400

    Update moonstrap.sh
    Added if statement for OS detection

--- moonstrap.sh
+++ moonstrap.sh
@@ -20,49 +20,56 @@ echo "This script written by Rainbow."
 echo "Follow me on Twitter (or don't. I don't really care.) @RainbowHacks"
 echo "The original script can always be found at http://cloudsdale.ponix.space/~rainbow/scripts/palemoonfbsd/moonstrap.sh"                                         
 
-echo "Bootstrapping build environment"
-echo "Using build dependancy list from http://www.freshports.org/www/firefox/"
-echo "Calling pkgng: "
+if [ "$(uname)" == "Darwin" ]; then
+    echo "OS X IS NOT A BSD. STAHP."      
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "This script will not function under Linux. Please use something not awful, like FreeBSD"
+elif [ "$(expr substr $(uname -s) 1 5)" == "FreeBSD" ]; then
+    echo "Bootstrapping build environment"
+   echo "Using build dependancy list from http://www.freshports.org/www/firefox/"
+   echo "Calling pkgng: "
 
-pkg install devel/nspr security/nss devel/libevent2 audio/soundtouch print/harfbuzz \
-graphics/graphite2 audio/libvorbis multimedia/libvpx databases/sqlite3 databases/py-sqlite3 \
-multimedia/v4l_compat devel/autoconf213 archivers/zip archivers/unzip devel/libnotify \
-devel/gmake devel/pkgconf lang/python27 devel/desktop-file-utils graphics/cairo graphics/libGL \
-x11/glproto x11/dri2proto x11/libXext x11/libXrender x11-toolkits/libXt multimedia/gstreamer1-plugins-good \
-multimedia/gstreamer1-libav lang/perl5.20 lang/gcc49 shells/bash devel/py-virtualenv devel/git \
- devel/py-gobject devel/dbus devel/yasm
+   pkg install devel/nspr security/nss devel/libevent2 audio/soundtouch print/harfbuzz \
+   graphics/graphite2 audio/libvorbis multimedia/libvpx databases/sqlite3 databases/py-sqlite3 \
+   multimedia/v4l_compat devel/autoconf213 archivers/zip archivers/unzip devel/libnotify \
+   devel/gmake devel/pkgconf lang/python27 devel/desktop-file-utils graphics/cairo graphics/libGL \
+   x11/glproto x11/dri2proto x11/libXext x11/libXrender x11-toolkits/libXt multimedia/gstreamer1-plugins-good \
+   multimedia/gstreamer1-libav lang/perl5.20 lang/gcc49 shells/bash devel/py-virtualenv devel/git \
+   devel/py-gobject devel/dbus devel/yasm
 
-echo "Installing Library Dependancies"
+   echo "Installing Library Dependancies"
 
-pkg install multimedia/libv4l graphics/cairo devel/libevent2 devel/libffi graphics/graphite2 \
-print/harfbuzz textproc/hunspell devel/icu devel/nspr security/nss graphics/png x11/pixman \
-audio/soundtouch databases/sqlite3 multimedia/libvpx audio/libvorbis devel/dbus-glib \
-x11/startup-notification audio/alsa-lib converters/libiconv graphics/jpeg accessibility/atk \
-devel/glib20 x11-toolkits/gtk20 x11-toolkits/pango
+   pkg install multimedia/libv4l graphics/cairo devel/libevent2 devel/libffi graphics/graphite2 \
+   print/harfbuzz textproc/hunspell devel/icu devel/nspr security/nss graphics/png x11/pixman \
+   audio/soundtouch databases/sqlite3 multimedia/libvpx audio/libvorbis devel/dbus-glib \
+   x11/startup-notification audio/alsa-lib converters/libiconv graphics/jpeg accessibility/atk \
+   devel/glib20 x11-toolkits/gtk20 x11-toolkits/pango
 
-echo "Modifying /etc/make.conf as Pale Moon will not build correctly with Clang"
-echo "CC=gcc49" > /etc/make.conf
-echo "CPP=cpp49" >> /etc/make.conf
-echo "CXX=g++49" >> /etc/make.conf
-echo "USE_GCC=gcc49" >> /etc/make.conf
+   echo "Modifying /etc/make.conf as Pale Moon will not build correctly with Clang"
+   echo "CC=gcc49" > /etc/make.conf
+   echo "CPP=cpp49" >> /etc/make.conf
+   echo "CXX=g++49" >> /etc/make.conf
+   echo "USE_GCC=gcc49" >> /etc/make.conf
 
-echo "Setting up build environment in /root"
+   echo "Setting up build environment in /root"
 
-mkdir /usr/home
-ln -s /usr/home /home
-ln -s /root /usr/home/root
-mkdir /usr/home/root/pmbuild
-cd /root
-fetch --no-verify-peer https://raw.githubusercontent.com/RainbowHackz/Moonstrap/Moonstrap-0.5/mozconfig.txt
-fetch --no-verify-peer https://raw.githubusercontent.com/RainbowHackz/Moonstrap/Moonstrap-0.5/build.sh
-chmod +x build.sh
-echo "Grabbing source from Git Branch..."
-git clone https://github.com/MoonchildProductions/Pale-Moon.git pmsrc
-cd pmsrc
-autoconf
-cd ..
-chmod -R +x pmsrc/
-./pmsrc/configure
-echo "Building! Cross your fingers!"
-#bash ./autobuild.sh
-bash ./build.sh
+   mkdir /usr/home
+   ln -s /usr/home /home
+   ln -s /root /usr/home/root
+   mkdir /usr/home/root/pmbuild
+   cd /root
+   fetch --no-verify-peer https://raw.githubusercontent.com/RainbowHackz/Moonstrap/Moonstrap-0.5/mozconfig.txt
+   fetch --no-verify-peer https://raw.githubusercontent.com/RainbowHackz/Moonstrap/Moonstrap-0.5/build.sh
+   chmod +x build.sh
+   echo "Grabbing source from Git Branch..."
+   git clone https://github.com/MoonchildProductions/Pale-Moon.git pmsrc
+   #cd pmsrc
+   #autoconf
+   #cd ..
+   chmod -R +x pmsrc/
+   #./pmsrc/configure
+   echo "Building! Cross your fingers!"
+   #bash ./autobuild.sh
+   bash ./build.sh
+fi
+
