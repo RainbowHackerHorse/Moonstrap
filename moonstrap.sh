fa92c0eade0bbfcbc2af25a374ac0a3dcfbb7f98
Rainbow <samantha@smsittech.com>
2016-04-06 03:30:22 -0400

    Update moonstrap.sh
    added 32-bit devel packages

--- moonstrap.sh
+++ moonstrap.sh
@@ -41,7 +41,7 @@ case "$(uname -s)" in
 			wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 			rpm -ivh epel-release-6-8.noarch.rpm
 			yum update -y
-			yum install -y autoconf213 yasm mesa-libGL-devel alsa-lib-devel libXt-devel gstreamer-devel gstreamer-plugins-base-devel pulseaudio-libs-devel
+			yum install -y glibc-devel.i686 glibc-devel ibstdc++-devel.i686 autoconf213 yasm mesa-libGL-devel alsa-lib-devel libXt-devel gstreamer-devel gstreamer-plugins-base-devel pulseaudio-libs-devel
 			yum groupinstall -y 'Development Tools' 'GNOME Software Development'
 			yum install -y zlib-devel openssl-devel sqlite-devel bzip2-devel # dependencies
 			wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
